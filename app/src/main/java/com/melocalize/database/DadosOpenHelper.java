86b0fef7650eb7df653bb8944f3fb27d24866161
pauloprevilato <53615485+pauloprevilato@users.noreply.github.com>
2020-10-15 11:00:22 -0400

    Add files via upload

---
+++ app/src/main/java/com/melocalize/database/DadosOpenHelper.java
@@ -0,0 +1,20 @@
+package com.melocalize.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DadosOpenHelper extends SQLiteOpenHelper {
+
+    public DadosOpenHelper(@Nullable Context context) {
+        super(context, "DADOS", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+}
